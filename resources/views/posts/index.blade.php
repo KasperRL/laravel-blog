9b5e148f1ec66c73b969fadc79f97b5f32e6bc5a
Kasper Ligthart <kasper.ligthart@student.rocva.nl>
2021-11-11 20:33:26 +0100

    initial commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    @include('posts._header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-post-grid :posts="$posts" />
+        @else
+            <p class="text-center">There are no posts here yet. Maybe check back later?</p>
+        @endif
+    </main>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts.index', [
+        'posts' => $author->posts
+    ]);
+});
