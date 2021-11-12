7c564866ea4927305e881479a62c55dc0fd62a90
Kasper Ligthart <kasper.ligthart@student.rocva.nl>
2021-11-12 18:49:33 +0100

    comments

--- database/migrations/2021_11_12_165345_create_comments_table.php
+++ database/migrations/2021_11_12_165345_create_comments_table.php
@@ -15,12 +15,10 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('post_id');
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
-
-            $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
         });
     }
 
