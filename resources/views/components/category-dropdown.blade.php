f85729e256c58f7cde2aa1d28a1f5431ded5372f
Kasper Ligthart <kasper.ligthart@student.rocva.nl>
2021-11-13 22:00:01 +0100

    fix for category dropdown active state

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,12 +7,12 @@
         </button>
     </x-slot>
     
-    <x-dropdown-item href="/?{{ http_build_query(request()->except('category', 'page')) }}" :active="request()->routeIs('home')">All</x-dropdown-item>
+    <x-dropdown-item href="/?{{ http_build_query(request()->except('category', 'page')) }}" :active="empty(request()->query('category'))">All</x-dropdown-item>
 
     @foreach ($categories as $category)
         <x-dropdown-item 
             href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
-            :active='request()->is("categories/$category->slug")'
+            :active="request()->query('category') == $category->slug"
         >{{ ucwords($category->name) }}</x-dropdown-item>
     @endforeach
 </x-dropdown>
