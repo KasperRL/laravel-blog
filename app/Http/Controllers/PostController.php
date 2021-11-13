3528d69d171f1a1591d7a08d2a7038f0f0d6af2d
Kasper <kasper.ligthart@icloud.com>
2021-11-13 13:33:25 +0100

    Add post as admin feature

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
@@ -14,10 +16,36 @@ class PostController extends Controller
             'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6)->withQueryString(),
         ]);
     }
+
     public function show(Post $post)
     {
         return view("posts.show", [
             'post' => $post
         ]);
+    }
+    
+    public function create()
+    {
+        return view("posts.create");
+    }
+
+    public function store()
+    {
+        $path = request()->file('thumbnail')->store('thumbnails');
+
+        return 'Done: ' . $path;
+        // $attributes = request()->validate([
+        //     'title' => ['required'],
+        //     'slug' => ['required', Rule::unique('posts', 'slug')],
+        //     'excerpt' => ['required'],
+        //     'body' => ['required'],
+        //     'category_id' => ['required', Rule::exists('categories', 'id')]
+        // ]);
+
+        // $attributes['user_id'] = auth()->user()->id;
+
+        // Post::create($attributes);
+
+        // return redirect('/')->with('success', 'Post published!');
     }
 }
